ec408d287db8892d26d5523eeec00f7362b62952
Walter Alpízar <walpizar@hotmail.com>
2019-05-30 10:15:20 -0600

    Crear proyecto de utilidad, entidades de factura electronicay enumeradores

---
+++ AppFacturadorApi.FacturaElectronica/ClasesDatos/Receptor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFacturadorApi.FacturaElectronica.ClasesDatos
+{
+    public class Receptor
+    {
+        public string Nombre { get; set; }
+        public string Identificacion_Tipo { get; set; }
+        public string Identificacion_Numero { get; set; }
+        public string Telefono_CodigoPais { get; set; }
+        public int Telefono_Numero { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        public Receptor(string pNombre, string pIdentificacion_Tipo,
+                        string pIdentificacion_Numero, string pUbicacion_Provincia,
+                        string pUbicacion_Canton, string pUbicacion_Distrito,
+                        string pUbicacion_Barrio, string pUbicacion_OtrasSenas,
+                        string pTelefono_CodigoPais, int pTelefono_Numero,
+                        string pCorreoElectronico)
+        {
+            Nombre = pNombre;
+            Identificacion_Tipo = pIdentificacion_Tipo;
+            Identificacion_Numero = pIdentificacion_Numero;
+            Telefono_CodigoPais = pTelefono_CodigoPais;
+            Telefono_Numero = pTelefono_Numero;
+            CorreoElectronico = pCorreoElectronico;
+        }
+
+    }
+}
---
+++ AppFacturadorApi.FacturaElectronica/Excepciones/GenerarXMLException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFacturadorApi.FacturaElectronica.Excepciones
+{
+    public class GenerarXMLException: Exception
+    {
+
+        public GenerarXMLException() : base()
+        {
+
+        }
+        public GenerarXMLException(Exception ex) :
+           base(ex.Message)
+        {
+
+        }
+    }
+}
