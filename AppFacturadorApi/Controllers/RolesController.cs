fbb4959de8228e6338ae4d55d3b9365cb187fb25
Kevin Zuñiga <Darktobi1616@hotmail.com>
2019-06-24 09:39:28 -0600

    Cambios registrocontroller

---
+++ AppFacturadorApi/Controllers/RolesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppFacturadorApi.Entities.Model;
+using AppFacturadorApi.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppFacturadorApi.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+            
+        IService<TbRoles> _RolesIns;
+
+        public RolesController(IService<TbRoles> RolesIns)
+        {
+            _RolesIns = RolesIns;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TbRoles>> Get()
+        {
+
+            try
+            {
+                IEnumerable<TbRoles> listaRoles = _RolesIns.ConsultarTodos();
+
+                if (listaRoles.ToList().Count == 0)
+                {
+                    return NotFound();
+                }
+                return Ok(listaRoles);
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500);
+            }
+        }
+
+
+    }
+}
